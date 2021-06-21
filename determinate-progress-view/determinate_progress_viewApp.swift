5ed6dbefb9791262ae2de3a816ee38578d6b9cc5
Paul Denlinger <pmdenlinger@icloud.com>
2021-06-21 16:29:23 -0700

    Initial Commit

---
+++ determinate-progress-view/determinate_progress_viewApp.swift
@@ -0,0 +1,17 @@
+//
+//  determinate_progress_viewApp.swift
+//  determinate-progress-view
+//
+//  Created by user on 6/21/21.
+//
+
+import SwiftUI
+
+@main
+struct determinate_progress_viewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
