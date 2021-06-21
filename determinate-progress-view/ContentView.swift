08f63c885b3755a5d637a12a63547e4e1850a5fe
Paul Denlinger <pmdenlinger@icloud.com>
2021-06-21 16:33:37 -0700

    Got this mixed up with indeterminate. THIS IS INDETERMINATE because it does not show amount of progress, while the other does.

--- determinate-progress-view/ContentView.swift
+++ determinate-progress-view/ContentView.swift
@@ -9,7 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        
+        ProgressView()
+        
+            .progressViewStyle(CircularProgressViewStyle(tint:.red))
             .padding()
     }
 }
